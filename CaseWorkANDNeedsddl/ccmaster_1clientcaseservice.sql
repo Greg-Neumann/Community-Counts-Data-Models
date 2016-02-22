573121c773a104193ddc1373ea706be225ec2990
Greg Neumann <greg@nsfwd.co.uk>
2016-02-22 12:48:21 +0000

    Correct RI link to 1servicetypes instead of servicetypes table

--- CaseWorkANDNeedsddl/ccmaster_1clientcaseservice.sql
+++ CaseWorkANDNeedsddl/ccmaster_1clientcaseservice.sql
@@ -7,5 +7,5 @@ CREATE TABLE `1clientcaseservice` (
   KEY `fk_1ClientCaseDetail_1ClientCaseHeader1_idx` (`idClientCaseHeader`),
   KEY `fk_1ClientCaseDetail_ServiceTypes1_idx` (`ServiceTypesid`),
   CONSTRAINT `fk_1ClientCaseDetail_1ClientCaseHeader1` FOREIGN KEY (`idClientCaseHeader`) REFERENCES `1clientcaseheader` (`idClientCaseHeader`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_1ClientCaseDetail_1ServiceTypes1` FOREIGN KEY (`ServiceTypesid`) REFERENCES `servicetypes` (`idServiceType`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_1ClientCaseDetail_1ServiceTypes1` FOREIGN KEY (`ServiceTypesid`) REFERENCES `1servicetypes` (`idServiceType`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
